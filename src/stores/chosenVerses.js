c5587ec90d966caa220523011467ed1e20c64c6a
JustRollingDevs <justrollingdevs@protonmail.com>
2023-02-05 09:27:40 +0200

    moved API_URL to .env file

--- src/stores/chosenVerses.js
+++ src/stores/chosenVerses.js
@@ -1,35 +1,37 @@
-import { defineStore } from 'pinia'
-import axios from "axios"
+import { defineStore } from 'pinia';
+import axios from 'axios';
 
-export const useChosenVerseStore = defineStore("chosenVerse",{
-    state: () => ({
-      chosenVerses: [],
-    }),
-    getters: {
-      getChosenVerses(state) {
-        return state.chosenVerses;
+export const useChosenVerseStore = defineStore('chosenVerse', {
+  state: () => ({
+    chosenVerses: [],
+  }),
+  getters: {
+    getChosenVerses(state) {
+      return state.chosenVerses;
+    },
+  },
+  actions: {
+    async fetchChosenVerses() {
+      try {
+        const req = await axios.get(
+          `${import.meta.env.VITE_API_URL}/chosen_verses`
+        );
+        this.chosenVerses = req.data;
+      } catch (error) {
+        alert(error);
+        console.log(error);
       }
     },
-    actions: {
-      async fetchChosenVerses() {
-        try {
-          const req = await axios.get('http://localhost:8000/api/chosen_verses')
-            this.chosenVerses = req.data
-          }
-          catch (error) {
-            alert(error)
-            console.log(error)
-        }
-      },
-      async fetchRandomChosenVerses(num) {
-        try {
-          const req = await axios.get(`http://localhost:8000/api/chosen_verses/random/${num}`)
-          this.chosenVerses = req.data
-        }
-        catch (error) {
-          alert(error)
-          console.log(error)
-        }
+    async fetchRandomChosenVerses(num) {
+      try {
+        const req = await axios.get(
+          `${import.meta.env.VITE_API_URL}/chosen_verses/random/${num}`
+        );
+        this.chosenVerses = req.data;
+      } catch (error) {
+        alert(error);
+        console.log(error);
       }
     },
-})
+  },
+});
--- src/stores/poems.js
+++ src/stores/poems.js
@@ -1,57 +1,54 @@
-import { defineStore } from 'pinia'
-import axios from "axios"
+import { defineStore } from 'pinia';
+import axios from 'axios';
 
-export const usePoemStore = defineStore("poem",{
-    state: () => ({
-      poems: [],
-      poem: {}
-    }),
-    getters: {
-      getPoems(state) {
-        return state.poems
-      },
-      getPoem(state) {
-        return state.poem
-      },
+export const usePoemStore = defineStore('poem', {
+  state: () => ({
+    poems: [],
+    poem: {},
+  }),
+  getters: {
+    getPoems(state) {
+      return state.poems;
     },
-    actions: {
-      async fetchPoems() {
-        try {
-          let apiPoemsIntros = "http://localhost:8000/api/poems_intros";
-          let reqPoemsIntros = await axios.get(apiPoemsIntros);
-          this.poems = reqPoemsIntros.data;
-        }
-        catch (error) {
-          alert(error)
-          console.log(error)
-        }
-      },
-      async fetchPoemAndOtherPoems(id) {
-        try {
-          let apiPoem = "http://localhost:8000/api/poem/" + id;
-          let reqPoem = await axios.get(apiPoem);
-          this.poem = reqPoem.data;
-          
-          this.fetchOtherPoems(id);
-        }
-        catch (error) {
-          alert(error)
-          console.log(error)
-        }
-      },
-      async fetchOtherPoems(id) {
-        try {
-          let apiPoemsIntros = "http://localhost:8000/api/poems_intros";
-          let reqPoemsIntros = await axios.get(apiPoemsIntros);
-          // didn't use indexOf because of the nature of mySQL indexes
-          // and we decreament it by 1 to match the 0 indexing of arrays
-          reqPoemsIntros.data.splice(id - 1, 1);
-          this.poems = reqPoemsIntros.data;
-        }
-        catch(error) {
-          alert(error);
-          console.log(error);
-        }
+    getPoem(state) {
+      return state.poem;
+    },
+  },
+  actions: {
+    async fetchPoems() {
+      try {
+        let apiPoemsIntros = `${import.meta.env.VITE_API_URL}/poems_intros`;
+        let reqPoemsIntros = await axios.get(apiPoemsIntros);
+        this.poems = reqPoemsIntros.data;
+      } catch (error) {
+        alert(error);
+        console.log(error);
       }
     },
-})
+    async fetchPoemAndOtherPoems(id) {
+      try {
+        let apiPoem = `${import.meta.env.VITE_API_URL}/poem/${id}`;
+        let reqPoem = await axios.get(apiPoem);
+        this.poem = reqPoem.data;
+
+        this.fetchOtherPoems(id);
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    },
+    async fetchOtherPoems(id) {
+      try {
+        let apiPoemsIntros = `${import.meta.env.VITE_API_URL}/poems_intros`;
+        let reqPoemsIntros = await axios.get(apiPoemsIntros);
+        // didn't use indexOf because of the nature of mySQL indexes
+        // and we decreament it by 1 to match the 0 indexing of arrays
+        reqPoemsIntros.data.splice(id - 1, 1);
+        this.poems = reqPoemsIntros.data;
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    },
+  },
+});
